27d91d742b161afdf33ed7bfb9af69598129ca9f
Arrasador3 <f92esmup@gmail.com>
2024-12-13 13:51:20 +0100

    updated

--- Image_detection/dependecies.sh
+++ Image_detection/dependecies.sh
@@ -1,30 +1,48 @@
 #!/bin/bash
 
-# Create a virtual environment
-python3.9 -m venv imagedetection
+set -e  # Exit immediately if a command exits with a non-zero status
+set -x  # Print commands and their arguments as they are executed
 
-# Activate the virtual environment
-source imagedetection/bin/activate
+echo "Creating a virtual environment in the user's home directory..."
+python3.9 -m venv ~/imagedetection
 
-# Upgrade pip
+echo "Activating the virtual environment..."
+source ~/imagedetection/bin/activate
+
+echo "Upgrading pip..."
 pip install --upgrade pip
 
-# Install dependencies from requirements.txt
+echo "Installing dependencies from requirements.txt..."
 pip install -r requirements.txt -f https://download.pytorch.org/whl/cu117/torch_stable.html
 
-# Install mmcv-full
+echo "Installing mmcv-full..."
 mim install mmcv-full
 
-# Crea las carpetas input y output
+echo "Creating directories..."
 mkdir -p ./weights/
 
-
-# Add weights
+echo "Updating apt-get and installing necessary packages..."
 apt-get update && apt-get install -y wget unzip python3-pip
-pip3 install gdown
-gdown --id 1n9UtHgfOA6H8cxp4Y44fG7OdXbVJzMnJ -O ./weights/work_dirs.zip
-unzip ./weights/work_dirs.zip -d ./weights/
-rm ./weights/work_dirs.zip
-gdown --id 1cIWM7lTisd1GajDR98IymDssvvLAKH1n -O ./weights/weights.pth
 
-###AUN TENGO QUE CORREGIRLO. NO ESTÁ BIEN DEL TODO#####
+echo "Installing gdown..."
+pip3 install gdown
+
+if [ ! -d ./weights/work_dirs ]; then
+    echo "Downloading weights..."
+    gdown --id 1n9UtHgfOA6H8cxp4Y44fG7OdXbVJzMnJ -O ./weights/work_dirs.zip
+
+    echo "Unzipping weights..."
+    unzip ./weights/work_dirs.zip -d ./weights/
+    rm ./weights/work_dirs.zip
+else
+    echo "Weights already downloaded."
+fi
+
+if [ ! -f ./weights/weights.pth ]; then
+    echo "Downloading additional weights..."
+    gdown --id 1cIWM7lTisd1GajDR98IymDssvvLAKH1n -O ./weights/weights.pth
+else
+    echo "Additional weights already downloaded."
+fi
+
+echo "Setup complete!"
