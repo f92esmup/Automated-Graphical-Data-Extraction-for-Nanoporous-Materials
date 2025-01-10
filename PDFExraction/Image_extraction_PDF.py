45cd94944918ea40b06ca6d87bd94aa8785ff355
f92esmup <f92esmup@gmail.com>
2025-01-10 15:06:39 +0100

    He empezado con PDFExtraction

--- PDFExraction/Image_extraction_PDF.py
+++ PDFExraction/Image_extraction_PDF.py
@@ -4,6 +4,8 @@ from PIL import Image
 import os
 
 def clear_output_folder(output_folder):
+    if not os.path.exists(output_folder):
+        os.makedirs(output_folder)
     for filename in os.listdir(output_folder):
         file_path = os.path.join(output_folder, filename)
         try:
@@ -14,6 +16,10 @@ def clear_output_folder(output_folder):
 
 def extract_images_from_pdf(pdf_path, output_folder):
     pdf_document = fitz.open(pdf_path)
+    pdf_name = os.path.splitext(os.path.basename(pdf_path))[0]
+    pdf_output_folder = os.path.join(output_folder, pdf_name)
+    if not os.path.exists(pdf_output_folder):
+        os.makedirs(pdf_output_folder)
     for page_number in range(len(pdf_document)):
         page = pdf_document.load_page(page_number)
         images = page.get_images(full=True)
@@ -23,10 +29,14 @@ def extract_images_from_pdf(pdf_path, output_folder):
             image_bytes = base_image['image']
             image_ext = base_image['ext']
             image = Image.open(io.BytesIO(image_bytes))
-            image.save(f"{output_folder}/image_{page_number+1}_{img_index+1}.{image_ext}")
+            image.save(f"{pdf_output_folder}/image_{page_number+1}_{img_index+1}.{image_ext}")
 
 # Ejemplo de uso
-pdf_path = './data/papers/reference.pdf'
+papers_folder = './data/papers'
 output_folder = './data/output'
 clear_output_folder(output_folder)
-extract_images_from_pdf(pdf_path, output_folder)
+
+for pdf_filename in os.listdir(papers_folder):
+    if pdf_filename.endswith('.pdf'):
+        pdf_path = os.path.join(papers_folder, pdf_filename)
+        extract_images_from_pdf(pdf_path, output_folder)
